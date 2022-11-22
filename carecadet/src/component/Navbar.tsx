565d64e178b12291408b0b23ecd2c3075d063586
geetha <gbaluft@gmail.com>
2022-11-22 14:03:48 +0530

    navbar

---
+++ carecadet/src/component/Navbar.tsx
@@ -0,0 +1,122 @@
+import React, { FC, ReactElement } from "react";
+import {
+  Box,
+  Link,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import { routes } from "../routes";
+import { NavLink } from "react-router-dom";
+
+const Navbar: FC = (): ReactElement => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event: any) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "auto",
+        backgroundColor: "secondary.main",
+      }}
+    >
+      <Container maxWidth="xl">
+      
+        <Toolbar disableGutters>
+          <Typography
+            variant="h5"
+            noWrap
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+         <Box sx={{fontWeight:"bold"}}>CareCadet</Box> 
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            
+            <Menu         
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ 
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {routes.map((page) => (
+                <Link
+                  key={page.key}
+                  component={NavLink}
+                  to={page.path}
+                  color="black"
+                  underline="none"
+                  variant="button"
+                >
+                  <MenuItem onClick={handleCloseNavMenu} sx={{width:250}}>
+                    <Typography textAlign="center">{page.title}</Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{   display: { xs: "flex", md: "none" } }}
+          >
+          <Box sx ={{flexGrow: "1", cursor: "pointer",display: { xs: "flex", md: "none" }}}>CareCadet</Box>
+          </Typography>
+          <Box sx={{  display: { xs: "none", md: "flex" } }}>
+            <Box sx= {{  marginLeft: {md:'25em',l:'45em',xl:'55em'}, display: "flex"}}>
+             {routes.map((page) => (
+                <Link
+                  key={page.key}
+                  component={NavLink}
+                  to={page.path}
+                  color="black"
+                  underline="none"
+                  variant="button"
+                  sx={{ fontSize: "large", marginLeft: "2rem" }}
+                >
+                  {page.title}
+                </Link>
+              ))}
+            </Box>
+          </Box>
+        </Toolbar>
+      </Container>
+    </Box>
+  );
+};
+
+export default Navbar;
